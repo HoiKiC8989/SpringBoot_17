36f9f16513305e4f7d0a81616dd3539ee4b9a772
HoiKiC8989 <yvoggy@gmail.com>
2018-03-14 11:55:32 -0400

    SpringBoot_18 Completed

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -11,6 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception{
 
